aeedf5854f920e3e986f7e07a66bec50ae4fef10
Filippo DallOlio <fdallolio@brocku.ca>
2025-02-02 01:54:25 -0500

    .

--- R/list_simplify.R
+++ R/list_simplify.R
@@ -19,18 +19,7 @@ list_simplify <- function (x, ..., strict = TRUE) {
   out
 }
 
-#' Flatten a list
-#' @export
-list_flatten <- function (x, ... ){
-  #name_spec = "{outer}_{inner}",
-  #name_repair = c("minimal", "unique", "check_unique", "universal")) {
-  stopifnot(obj_is_list(x))
-  stopifnot(...length() == 0)
-  i <- vapply(x, obj_is_list, logical(1))
-  x[i] <- lapply(x[i],unclass)
-  x[!i] <- lapply(x[!i],list)
-  unlist(x, recursive = FALSE)
-}
+
 
 
 
